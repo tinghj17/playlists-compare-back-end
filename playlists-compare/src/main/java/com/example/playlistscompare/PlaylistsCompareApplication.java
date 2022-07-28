731779bf51dc20ec215f54e099ee328dcc7498d4
Tinghui <63171628+tinghj17@users.noreply.github.com>
2022-07-27 17:49:44 -0700

    Add files via upload

---
+++ playlists-compare/src/main/java/com/example/playlistscompare/PlaylistsCompareApplication.java
@@ -0,0 +1,13 @@
+package com.example.playlistscompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistsCompareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistsCompareApplication.class, args);
+	}
+
+}
---
+++ playlists-compare/src/test/java/com/example/playlistscompare/PlaylistsCompareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playlistscompare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistsCompareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
