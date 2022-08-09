96ae354b6ac3b514c28e08e444d91529c6acd711
tinghj17 <tinghj17@gmail.com>
2022-08-09 12:53:55 -0700

    delete tutorial project

--- src/main/java/tutorial/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package tutorial.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tutorial.model.User;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
