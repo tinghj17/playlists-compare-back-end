db6491e2260cebca9cc68090face138d022365ee
tinghj17 <tinghj17@gmail.com>
2022-08-09 21:49:35 -0700

    comment Dotenv out

--- src/main/java/com/example/playlistscompare/controller/PlaylistController.java
+++ src/main/java/com/example/playlistscompare/controller/PlaylistController.java
@@ -1,7 +1,7 @@
 package com.example.playlistscompare.controller;
 
 import com.example.playlistscompare.repository.PlaylistRepository;
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import se.michaelthelin.spotify.SpotifyApi;
@@ -36,7 +36,7 @@ public class PlaylistController {
     //    http://localhost:8080/api/general-info
     @GetMapping(value = "general-info")
     public static ArrayList<HashMap> getPlaylistsInfo() {
-        Dotenv dotenv = Dotenv.configure().filename("env").load();
+//        Dotenv dotenv = Dotenv.configure().filename("env").load();
 //        String ClientId = dotenv.get("CLIENT_ID");
 //        String ClientSecret = dotenv.get("CLIENT_SECRET");
         String ClientId = System.getenv("CLIENT_ID");
